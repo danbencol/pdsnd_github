661abacedf071eb7ab8226552f62143ab2960950
DanielBendel <danielbendel@gmx.de>
2024-10-17 15:47:55 +0200

    made greeting more formal by removing 'Hello' from it

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -28,7 +28,7 @@ def get_filters() -> Tuple[str, str, str]:
             - month: name of the month to filter by, or "all" to apply no month filter
             - day: name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Let\'s explore some US bikeshare data!')
 
     # Get user input for city
     while True:
